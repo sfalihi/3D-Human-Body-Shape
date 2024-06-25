0794c5f5fda232dd8335b4ac587c0c3f0ef7fac0
sf <sn.falihi@gmail.com>
2024-06-25 13:10:37 +0330

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
+source .env
+
+git pull
+
 sudo docker compose config > docker-compose.processed.yml
 
 sed -i '/published:/ s/"//g' docker-compose.processed.yml
 sed -i '/^name: .*/d' docker-compose.processed.yml
+
+sudo docker stack deploy -c ./docker-compose.processed.yml "$STACK_NAME"
